8026f8ca9b2e99671f0ece9d71502b4d9d707a6f
sumiaoqing815 <sumiaoqing815@gmail.com>
2022-03-07 11:46:05 +0800

    拖拉操作

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="list-show">
+      <div class="list-show-item" draggable="able" @dragstart="dragStart(index)" @dragover="dragOver(index)" @dragend="dragEnd(index)" v-for="(item,index) in test_lst" :key="index">
+        {{item}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      checkedStatus: null,
+      test_lst: ['test1', 'test2', 'test3', 'test4', 'test5', 'test6'],
+      move_id: 'test1'
+    };
+  },
+  methods:
+  {
+    select(e) {
+      console.log(e.target.checked, '0000');
+    },
+    dragStart(index) {
+      this.move_id = index;
+      console.log(index);
+    },
+    dragOver(index) {
+      if (index == this.move_id) return
+      let temp = this.test_lst[this.move_id];
+      Vue.set(this.test_lst, this.move_id, this.test_lst[index]);
+      Vue.set(this.test_lst, index, temp);
+      console.log(index);
+
+    },
+    dragEnd() {
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.list-show {
+  width: 200px;
+  background: #cdcdcd;
+  height: auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.list-show-item {
+  height: 40px;
+  background: #345689;
+  width: 100%;
+  color: #fff;
+  border: 1px solid #fff;
+  box-sizing: border-box;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    css: {
+        modules: false, // 启用 CSS modules
+        extract: true, // 是否使用css分离插件
+        sourceMap: false, // 开启 CSS source maps?
+        loaderOptions: {
+            postcss: {
+                // 这是rem适配的配置  注意： remUnit在这里要根据lib-flexible的规则来配制，如果您的设计稿是750px的，用75就刚刚好。
+                 plugins: [
+                    require("postcss-px2rem-exclude")({
+                        remUnit: 75,
+                        exclude: /node_modules|folder_name/i
+                    })
+                ]
+            }
+        } // css预设器配置项
+    }
+}
